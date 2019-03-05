24d4555e1bf891830f7644fcb4b012c127600049
akmalist <akmalist@gmail.com>
2019-03-05 06:41:50 -0800

    level 4 Salting

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 //jshint esversion:6
 
 require('dotenv').config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -14,10 +17,14 @@ const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //create connection to mongoose
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
 //create schema
 
@@ -28,7 +35,7 @@ const userSchema = new mongoose.Schema({
 
 // create encrypt -mongoose
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] }); //use encrypt PLUGIN  they allow for applying pre-packaged capabilities to extend their functionality.
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] }); //use encrypt PLUGIN  they allow for applying pre-packaged capabilities to extend their functionality.
 //also we are Encrypted Only password Field
 
 
@@ -39,52 +46,66 @@ const User = mongoose.model("User", userSchema);
 
 
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login",function(req,res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register",function(req,res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
 
 //create post request
 
-app.post("/register", function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password:req.body.password
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
+app.post("/register", function(req, res) {
+
+  bcrypt.hash(req.body.password, 10, function(err, hash) { //using bcrypt security level 4
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
         res.render("secrets");
-    }
+      }
+    });
   });
+
 
 });
 
-app.post("/login",function(req,res){
+app.post("/login", function(req, res) {
   const username = req.body.username;
   const password = req.body.password; // whatever the user types
 
-User.findOne({email: username}, function(err, foundUser){
-  if(err){
-    console.log(err);
-  }else{
-     if(foundUser){ //do we have a user in our database?
-      if(foundUser.password === password) { // does the password the user types matches the one we have on our database?
-        res.render("secrets");
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) { //do we have a user in our database?
+        // if(foundUser.password === password) { // does the password the user types matches the one we have on our database?
+        bcrypt.compare(password, foundUser.password, function(err, resaultfromDB) { //using bcrypt security level 4
+          if (resaultfromDB === true) {
+            res.render("secrets");
+          }
+
+        });
+
+
       }
     }
-  }
 
-});
+  });
 });
 
 
@@ -97,6 +118,6 @@ User.findOne({email: username}, function(err, foundUser){
 
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server Started on port 3000");
 });
